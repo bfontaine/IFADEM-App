1b704de190e280ddb85ca01d3b7f0e53cf00b5ea
Baptiste Fontaine <batifon@yahoo.fr>
2013-06-18 17:51:44 +0200

    Popups closed on click.

--- js/app.js
+++ js/app.js
@@ -2,16 +2,21 @@ $(function() {
 
     // a click on an image shows a pop-up with some infos about a content,
     // while a click on the rest of the content (un)select it.
-    $(document.body).on( 'click', '.content *', function( e ) {
+    $(document.body).on( 'click', '.content', function( e ) {
 
-        var $content = $(e.target).parent(), $checkbox;
+        var nodeName = e.target.nodeName.toUpperCase(),
+            $content = nodeName == 'LI' ? $(e.target) : $(e.target).parent(),
+            $checkbox;
 
-        if (e.target.nodeName == 'IMG') {
-            $( '#content-details-' + $content.data( 'contentId' ) ).popup().popup( 'open' );
-            return;
+        if (nodeName == 'IMG') {
+            $( '#content-details-' + $content.data( 'contentId' ) )
+                .popup()
+                .popup( 'open' )
+                // close on click
+                .one( 'click', function() { $(this).popup( 'close' ); });
+        } else {
+            $content.toggleClass( 'selected' );
         }
-
-        $content.toggleClass( 'selected' );
 
     });
 
