fdbfdccdc76501cba84a8fdf8b5ae21aa2421ace
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-16 20:55:22 +0200

    Setting the default timezone to avoid a PHP Warning.

--- initialization.php
+++ initialization.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // settings
 include_once __DIR__ . '/config.php';
+date_default_timezone_set(TIMEZONE);
 
 // Twig initialization
 Twig_Autoloader::register();
