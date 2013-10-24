d3ac02d29fe382640ba66a85f23af6db6195c00b
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-24 23:12:41 +0200

    Test for AppCache support added.

--- js/resources.js
+++ js/resources.js
@@ -1,2 +1,15 @@
-var user = window.user;
+(function(w) {
+    var appCache = w.applicationCache
+                    || w.webkitApplicationCache
+                    || w.mozApplicationCache
+                    || w.msApplicationCache,
 
+        $content = $('#content');
+
+
+    if (!appcache) {
+        $content.html('<p>Le cache n\'est pas supporté par votre navigateur.</p>');
+        return;
+    }
+
+})(window);
